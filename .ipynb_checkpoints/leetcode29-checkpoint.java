15072ad62e53b7bdb94c0005cbbb9733fae09e75
yuanhang zhang <yz140212@ohio.edu>
2021-04-30 10:16:58 -0400

    4-30-21 update

---
+++ .ipynb_checkpoints/leetcode29-checkpoint.java
@@ -0,0 +1,12 @@
+public class leetcode29{
+  public static void main(String[] args){
+    int a = 2;
+    int b = 3;
+
+    //System.out.println(b >= 2);
+    //System.out.println(a << 1);
+    System.out.println(b >= a << 1);
+    System.out.println(a);
+    System.out.println(b);
+  }
+}
